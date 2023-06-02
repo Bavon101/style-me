58dd8280bd5513faa025e414d6a04cd0aac0960a
jmungai <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>
2023-06-02 18:34:04 +0300

    About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import './styles.scss';
+
+export default function Page() {
+  return (
+    <div className="about-wrapper py-12 px-4 sm:px-0">
+      <p className="heading">Style Me</p>
+      <p><strong>Style Me</strong> is an innovative AI-powered Virtual Stylist app designed to celebrate and empower the vibrant fashion culture of Africa. With a unique focus on Africa's diverse fashion landscape, Style Me combines the latest AI and computer vision advancements to provide a personalized and immersive fashion experience.
+      </p>
+      <p><strong>Discover the Essence of African Fashion: </strong>
+      Style Me takes you on a captivating journey through the rich tapestry of African fashion. From traditional attire to contemporary designs, the app showcases the unique styles, colours, and patterns that reflect the cultural heritage of various African countries. Embrace the beauty of African fashion and unleash your individuality.
+      </p>
+      <p><strong>Unleash Your Personal Style: </strong>
+      Style Me understands that fashion is a form of self-expression. Through its intelligent algorithms, the app analyzes your preferences, body type, and fashion inspirations to curate stylish outfit combinations that align with your personal style. Discover new ways to express yourself authentically and confidently.
+      </p>
+      <p><strong>Promote Sustainability and Ethical Fashion:</strong>
+      Style Me believes in fashion that cares for both people and the planet. The app encourages sustainable fashion choices by recommending eco-friendly materials, promoting local artisans, and connecting you with brands that prioritize fair trade and ethical production practices. Be a part of the movement towards a more sustainable fashion future.
+      </p>
+      <p><strong>Support Local African Designers: </strong>
+      At Style Me, we celebrate the immense talent of African fashion designers. The app provides a dedicated platform to showcase their creations and offers outfit suggestions that feature their unique designs. By supporting local designers, Style Me contributes to the growth of the African fashion industry and helps create economic opportunities.
+      </p>
+      <p><strong>Connect with African Fashion Retailers: </strong>
+      Discover a world of African fashion at your fingertips. Style Me connects you with a curated selection of African fashion retailers and e-commerce platforms. Seamlessly browse and purchase new wardrobe items that enhance your personal style while contributing to the growth of local economies.
+      </p>
+      <p><strong>Join the Style Me Community: </strong>
+      Style Me is more than just an app; it's a community of fashion enthusiasts passionate about African fashion. Share your looks, exchange styling tips, and connect with like-minded individuals across the continent. Together, let's celebrate and elevate African fashion to new heights.
+      Join Style Me now and embark on a fashion journey that embraces the essence of Africa, empowers local designers, and promotes sustainable fashion for a brighter future. Discover your style today!
+      </p>
+    </div>
+  );
+}
